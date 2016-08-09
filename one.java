3a6f8de65a6803b5f803c0d5f4efe3c2aacb79df
ManjuMuralidharan <manju1695@gmail.com>
2016-08-09 18:52:36 +0530

    Add files via upload

---
+++ one.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class one {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 
+			int a,b,c,largest;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter any three numbers: ");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=s.nextInt();
+
+		if(a>=b && a>=c) 
+		{
+		System.out.println("Largest number: "+a);
+		}
+		if(b>=a && b>=c) 
+		{
+		System.out.println("Largest number: "+b);
+		}
+		if(c>=a && c>=b) 
+		{
+		System.out.println("Largest number: "+c);
+		}
+        
+
+
+
+			}
+
+}
